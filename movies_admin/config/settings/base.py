9a62869b81cdb90125f4d19d5f29c6a365e096e6
Max Snegirev <goshasnegir@icloud.com>
2021-11-22 22:13:09 +0500

    Доработки и прикрутил nginx

--- movies_admin/config/settings/base.py
+++ movies_admin/config/settings/base.py
@@ -8,7 +8,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+# See https://docs.djangop
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
